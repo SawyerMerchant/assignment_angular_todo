c48c1ef5562a6271b22e3ae53a54362a6f14cefa
John Sawyer <sawyer@sawyermerchant.com>
2017-04-10 15:04:42 -0400

    Connect create button

--- javascripts/controllers/todoCtrl.js
+++ javascripts/controllers/todoCtrl.js
@@ -1,22 +1,28 @@
 toDo.controller('toDoCtrl',
-  ['$scope', function($scope) {
-    $scope.items = [
-      {
-        text: "Get groceries from the store",
-        dueDate: new Date(),
-        completed: false
-      },
-      {
-        text: "Get tools from the factory",
-        dueDate: new Date(),
-        completed: false
-      }
-    ];
+  ['$scope', '$window',
+    function($scope, $window){
+      $scope.items = [
+        {
+          text: "Get groceries from the store",
+          dueDate: new Date(),
+          completed: false
+        },
+        {
+          text: "Get tools from the factory",
+          dueDate: new Date(),
+          completed: false
+        }
+      ];
 
-    // $scope.item = {
-    //   text: "Get groceries from the store",
-    //   dueDate: new Date(),
-    //   completed: false
-    // };
-  }
+      $scope.createToDo = function(){
+        $window.alert("To doing");
+        // var newToDo = {
+        //   text: newOne.text,
+        //   dueDate: newOne.dueDate,
+        //   completed: false
+        // };
+        // $scope.items.push(newToDo);
+        // return true;
+      };
+    }
 ]);
