aa73a847d1325e33e49b6f104573b5bc1d584e80
John Sawyer <sawyer@sawyermerchant.com>
2017-04-10 13:54:01 -0400

    Fix ng repeat

--- javascripts/controllers/todoCtrl.js
+++ javascripts/controllers/todoCtrl.js
@@ -1,22 +1,22 @@
 toDo.controller('toDoCtrl',
   ['$scope', function($scope) {
-    // $scope.items = [
-    //   {
-    //     text: "Get groceries from the store",
-    //     dueDate: new Date(),
-    //     completed: false
-    //   },
-    //   {
-    //     text: "Get tools from the factory",
-    //     dueDate: new Date(),
-    //     completed: false
-    //   }
-    // ];
+    $scope.items = [
+      {
+        text: "Get groceries from the store",
+        dueDate: new Date(),
+        completed: false
+      },
+      {
+        text: "Get tools from the factory",
+        dueDate: new Date(),
+        completed: false
+      }
+    ];
 
-    $scope.item = {
-      text: "Get groceries from the store",
-      dueDate: new Date(),
-      completed: false
-    };
+    // $scope.item = {
+    //   text: "Get groceries from the store",
+    //   dueDate: new Date(),
+    //   completed: false
+    // };
   }
 ]);
